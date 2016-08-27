426c752aaad31db424c0aeac1e96b11075b2e8c0
harshal <harshal.carpenter@gmail.com>
2016-08-26 23:03:48 -0700

    Please

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 var http = require("http");
-var qs = require("querystring");
-var port = process.env.PORT || 3000;
+var url = require("url");
 var express = require("express");
 var app = express();
 app.use(express.json());
 
 //port for Heroku
-app.set('port', (process.env.PORT));
+app.set('port', (process.env.PORT || 5000));
 
 app.post('/liveh2h',function(req,res){
-	/*if(req.token === "bPQAKQ0fj4WHw37l8kIhwYZY"){
-		
-	}*/
+	var url = url.parse(req.url,true);
+	
 	res.send("Got Message");
 });
