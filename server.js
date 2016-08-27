1ec06abaadb5d33edf76eaa263c52bd1caae2d55
harshal <harshal.carpenter@gmail.com>
2016-08-26 23:08:10 -0700

    Funk

--- server.js
+++ server.js
@@ -6,9 +6,14 @@ app.use(express.json());
 
 //port for Heroku
 app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 app.post('/liveh2h',function(req,res){
 	var url = url.parse(req.url,true);
 	
 	res.send("Got Message");
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
 });
