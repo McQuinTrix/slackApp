d2a56c91874f1c30694151032f1e84fbfcd8915f
harshal <harshal.carpenter@gmail.com>
2016-08-26 23:25:35 -0700

    Procfile

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 var http = require("http");
 var url = require("url");
 var express = require("express");
+var request = require("request");
+var bodyParser = require('body-parser');
 var app = express();
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 //port for Heroku
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
-
+app.get('/',function(req,res){
+	res.send('Running');
+});
 app.post('/liveh2h',function(req,res){
-	var url = url.parse(req.url,true);
-	
+	var query = req.body.text;
 	res.send("Got Message");
 });
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+  console.log('Node app is running on port', app.get('port'));
 });
