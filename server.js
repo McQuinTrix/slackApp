110e97276b6b2297a6ee99568ba61dcd92d11b7d
harshal <harshal.carpenter@gmail.com>
2016-08-30 23:05:43 -0700

    Huzzah9

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var url = require("url");
 var express = require("express");
 var request = require("request");
 var bodyParser = require('body-parser');
+var request = require('require');
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -21,6 +22,12 @@ app.post('/liveh2h',function(req,res){
 			"response_type": 'in_channel',
 			"text": "Ooo..So you <@"+req.body.user_id+"|"+req.body.user_name+"> wanna create "+arr[1]+"! Lets do it with: <"+arr[2]+">"
 		}));
+		request.post(req.body.response_url,{json:{
+			"response_type": 'in_channel',
+			"text": "Ooo..So you <@"+req.body.user_id+"|"+req.body.user_name+"> wanna create "+arr[1]+"! Lets do it with: <"+arr[2]+">"
+		}},function(err,resp,body){
+			
+		})
 		//res.send("Ooo..So you <@"+req.body.user_id+"|"+req.body.user_name+"> wanna create "+arr[1]+"! Lets do it!"+req.body.text);
 	}else{
 		res.send("I am sorry I didn't quite catch that!");
