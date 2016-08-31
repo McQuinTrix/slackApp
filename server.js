339aced41e1e88c1eafc67591d84a6ab879c9448
harshal <harshal.carpenter@gmail.com>
2016-08-30 20:44:59 -0700

    Huzzah3

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/',function(req,res){
 app.post('/liveh2h',function(req,res){
 	var arr = req.body.text.split(" ");
 	if(arr[0] === "create"){
-		res.send("Ooo..So you "+req.body.user_name+" wanna create "+req[1]+"! Lets do it!");
+		res.send("Ooo..So you @"+req.body.user_name+" wanna create "+arr[1]+"! Lets do it!");
 	}else{
 		res.send("I am sorry I didn't quite catch that!");
 	}
