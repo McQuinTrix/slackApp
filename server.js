680c5dd644a0e0529fe1d4e42f29d84aadac7305
Harshal Carpenter <harshal.carpenter@gmail.com>
2016-09-17 20:00:41 -0700

    Authorize SQL BOT Errors

--- server.js
+++ server.js
@@ -44,8 +44,7 @@ app.get('/authorize',function(req,res){
     var code = req.query.code;
     console.log(req.query.code);
     request.post("https://slack.com/api/oauth.access?client_id=72362934594.72343901492&client_secret=774325bbe3f942efb71d5db978eb5a4b&code="+code,function(err,resp,body){
-        var json = body;
-        console.log("48:"+ typeof json);
+        var json = JSON.parse(body);
         var access_token = json.access_token,
             user_id = json.user_id,
             team_name = json.team_name,
