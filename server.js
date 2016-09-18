6edd8ffefff0f045aafe12c4b97d6b3c15bb8cc6
Harshal Carpenter <harshal.carpenter@gmail.com>
2016-09-18 14:54:33 -0700

    slack command lastr

--- server.js
+++ server.js
@@ -155,8 +155,8 @@ app.post('/liveh2h',function(req,res){
                             })
                             
                         }else if(elem[0] === "#"){
-                            var theID = meetingID.substring(0,3) + "-" + meetingID.substring(3,6)+ "-" + meetingID.substring(6);
-                            var gLink = "https://liveh2h.com/"+theID;
+                            //var theID = meetingID.substring(0,3) + "-" + meetingID.substring(3,6)+ "-" + meetingID.substring(6);
+                            var gLink = "https://liveh2h.com/"+meetingID;
                             PartURL += urlSlack + "&channel="+elem.substring(1);
                             PartURL += '&attachments=' + encodeURIComponent('[{"text":"Hello! '+req.body.user_name+' has created a meeting, and you all have been invited: <'+gLink+'|Click here to join>"}]')
                             request.post(PartURL);
