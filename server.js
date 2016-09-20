585c99c12955c84029da0e56ec99d5270e0aaea2
Harshal Carpenter <harshal.carpenter@gmail.com>
2016-09-20 13:47:01 -0700

    slack getting emails

--- server.js
+++ server.js
@@ -237,7 +237,7 @@ app.post('/liveh2h',function(req,res){
                     "text": ":small_blue_diamond:`/liveh2h [@username | #channel]` Create a meeting and invite others using username or channel name _(For example: `/liveh2h @john @mary #general`)_ \n:small_blue_diamond: `/liveh2h meetnow [@username | #channel]` Create a meeting and invite others using username or channel name _(For example: `/liveh2h meetnow @john @mary #general`)_ \n :small_blue_diamond: `/liveh2h join xxx-xxx-xxx` Join a meeting using 9-digit meeting id _(For example: `/liveh2h join 123456789` or `/liveh2h join 123-456-789`)_ \n :small_blue_diamond: `/liveh2h help` Lists available commands \n For more features, visit: <https://www.liveh2h.com/|LiveH2H.com>",
                     "footer": "LiveH2H",
                     "footer_icon": "https://s3-us-west-2.amazonaws.com/slack-files2/avatar-temp/2016-09-18/80976650579_59e903b677a8359139ab.png",
-                    "ts": Date.now()
+                    "ts": Math.floor((new Date).getTime()/1000) 
                 }]
                 
             }},function(err,resp,body){
