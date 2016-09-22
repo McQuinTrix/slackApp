205bcc4481b0428040ac464b512d09284c5d11bd
Harshal Carpenter <harshal.carpenter@gmail.com>
2016-09-22 14:29:53 -0700

    Redirect changes

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 //app.set('views', __dirname + '/');
-app.use(express.static(__dirname + '../public'));
+app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 
 /***************************/
